5fd74c0d662269bba3b8df4fcdcf4ad5d8a9cbe3
mozzi0131 <sue03094@gmail.com>
2020-05-07 17:15:12 +0900

    add 08_atoi.cc

--- leetcode/08_atoi.cc
+++ leetcode/08_atoi.cc
@@ -8,15 +8,34 @@ class Solution {
 public:
 
     int processNum(const string& str, bool isMinus){
+        cout << "first-processed string is " << str << ", and it's minus value is " << isMinus << endl;
         int INT_MIN = -pow(2, 31);
         int INT_MAX = pow(2, 31) - 1;
 
+        int seedNum = 1;
+        int result = 0;
+
         for(int i=0;i<str.length(); i++){
             
+
+            if(isMinus){
+                result -= seedNum * (str.at(i) - '0');
+            }
+            else{
+                result += seedNum * (str.at(i) - '0');
+            }
+
+            if(result < INT_MIN){
+                return INT_MIN;
+            }
+            else if(result >= INT_MAX){
+                return INT_MAX;
+            }
+
+            seedNum *= 10;
         }
         
-        cout << "first-processed string is " << str << ", and it's minus value is " << isMinus << endl;
-        return 0;
+        return result;
     }
 
     int myAtoi(const string& str) {
